dd4e73f95051e14d06446b81820503de5adc5c04
Amir <amirhossein.d.z@gmail.com>
2025-08-10 19:47:05 +0200

    add train-test split

--- train_linreg.py
+++ train_linreg.py
@@ -18,3 +18,7 @@ y = df['MedHouseVal']
 
 # quick EDA
 print(df.describe().T[['mean', 'std', 'min', 'max']])
+
+# train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.2, random_state=42)
